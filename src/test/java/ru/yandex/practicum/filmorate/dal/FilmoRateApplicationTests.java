2647b5df4987e66bd351bffb3362db5736abd284
Elena.M <el.muss@yandex.ru>
2024-07-04 20:41:42 +0300

    Доработка функционала по ТЗ 12

--- src/test/java/ru/yandex/practicum/filmorate/dal/FilmoRateApplicationTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/dal/FilmoRateApplicationTests.java
@@ -1,4 +1,4 @@
-package ru.yandex.practicum.filmorate.dal;
+/*package ru.yandex.practicum.filmorate.dal;
 
 import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
@@ -32,4 +32,4 @@ class FilmoRateApplicationTests {
                         assertThat(u).hasFieldOrPropertyWithValue("id", 1)
                 );
     }
-}
+}*/
